71c95c58c4172e9df329da72daca21e1db6b27e2
Albertzry <albertzry@qq.com>
2024-09-05 16:46:30 +0800

    add files

---
+++ 7 4_实现指定函数printSize/4_实现指定函数printSize.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void printSize(int* first, int* last) {
+    if (first == NULL || last==NULL || last - first <= 0) {
+        printf("error\n");
+        return;
+    }
+    int i = 0;
+    while ((first + i) != last) {
+        i++;
+    }
+    printf("%d,%d\n", i+1, 4 * (i+1));
+    return;
+}
---
+++ 7 4_实现指定函数printSize/README.md
@@ -0,0 +1,8 @@
+# 7 4_实现指定函数printSize
+实现printSize方法：
+void printSize(int * first,int * last);
+传入数组第一个元素地址和最后一个元素地址。
+输出数组内元素的个数和数组的大小。
+用逗号区分，回车结束输出。
+如输出4,16回车
+遇到异常情况，输出"error"。
