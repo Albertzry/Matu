7d4170c27de718e2fbf09200af72731c0490aa68
Albertzry <albertzry@qq.com>
2024-11-08 11:15:10 +0800

    upload

---
+++ 19 15_求1-n的阶乘之和/15_求1-n的阶乘之和.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int  n;
+	cin >> n;
+	if (n < 1 || n>10) {
+		cout << "error";
+		return 0;
+	}
+	int  i = 1, r = 1,z=1;
+	while (i <  n) {
+		i++;
+		z *= i;
+		r += z;
+	}
+	cout << r<<endl;
+	return 0;
+}
