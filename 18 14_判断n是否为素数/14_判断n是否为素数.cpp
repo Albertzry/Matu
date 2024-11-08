c38d0d6544bce4044cdf5cbbdbe32b3402d26aba
Albertzry <albertzry@qq.com>
2024-11-08 11:13:38 +0800

    upload

---
+++ 18 14_判断n是否为素数/14_判断n是否为素数.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long  n;
+	int tt = scanf("%ld", &n);
+	if (tt == 0||n<=3) {
+		cout << "error";
+		return 0;
+	}
+	bool t = 1;
+	for (int i = 2; i < sqrt(double(n)); i++) {
+		if (n % i == 0) {
+			t = 0;
+			break;
+		}
+	}
+	if (t == 1)
+		cout << "yes";
+	else
+		cout << "no";
+	return 0;
+}
